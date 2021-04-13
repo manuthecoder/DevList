9d58e5c7cd5cdab68e0bd20a64a5c45a43f6dbd7
ManuTheCoder <hello@homebase.rf.gd>
2021-04-13 13:48:53 -0700

    Add files via upload

---
+++ src/app/user/avatar.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include('../cred.php');
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "UPDATE accounts SET avatar=".json_encode($_POST['avatar'])." WHERE id=".$_SESSION['id'];
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
